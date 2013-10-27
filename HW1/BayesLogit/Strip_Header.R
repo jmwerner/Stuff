86063a7f09bd5b903502dd45fd9e72864e70b99e
jmwerner <jwerner@ucdavis.edu>
2013-10-27 12:15:25 -0700

    strip header function added

---
+++ HW1/BayesLogit/Strip_Header.R
@@ -0,0 +1,10 @@
+
+
+files = list.files()
+
+for(i in 1:length(files)){
+	temp = read.csv(files[i])
+	write.table(temp, file = files[i], row.names = FALSE, col.names = FALSE, sep = ",")
+}
+
+q("no")
